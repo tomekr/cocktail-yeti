b91f0c408b1c4b1db80dad98b60facf820575cea
Tomek Rabczak <tomek.rabczak@gmail.com>
2018-05-14 12:54:38 -0500

    Add StaticPages controller with basic home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def heartbeat
+    render plain: "Aroooooo!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-      mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-    end
+  root 'static_pages#home'
   post "/graphql", to: "graphql#execute"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/heartbeat", to: "application#heartbeat"
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
 end
