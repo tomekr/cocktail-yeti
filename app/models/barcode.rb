2f9beb019bec6b8c0617e6f8620763a3c69de54d
Tomek Rabczak <tomek.rabczak@gmail.com>
2018-05-24 17:07:19 -0700

    Validate presence of barcode

--- app/models/barcode.rb
+++ app/models/barcode.rb
@@ -1,3 +1,4 @@
 class Barcode < ApplicationRecord
   belongs_to :ingredient
+  validates :barcode, presence: true
 end
--- spec/models/barcode_spec.rb
+++ spec/models/barcode_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Barcode, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without a barcode" do
+    barcode = Barcode.new(barcode: nil, ingredient: build(:aperol))
+    expect(barcode).to_not be_valid
+  end
 end
